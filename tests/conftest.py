45fb47b8bc20b347d50238aba37c4909136b8e00
Alice Chen <alice.chen@slalom.com>
2019-04-04 13:03:21 -0400

    Added unit testing

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from pyspark.sql import SparkSession
+
+APP_NAME = "unit-testing"
+
+
+@pytest.fixture(scope = "session")
+def spark():
+    return SparkSession.builder.master("local[*]").appName(APP_NAME).getOrCreate()
